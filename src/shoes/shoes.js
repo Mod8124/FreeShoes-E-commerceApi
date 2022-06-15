8d72f0e8b9b901bfceb0b05e81f0765f0811657e
denis <denisenriquebaca@gmail.com>
2022-06-15 17:20:21 -0600

    modify img

--- src/shoes/shoes.js
+++ src/shoes/shoes.js
@@ -6,7 +6,7 @@ const genre = ['men','women']
 const prices = [250.00,50,125.00]
 
 const shoes = [
-    {id:0,name:"Fall Limited Edition Sneakers",price:prices[0].toFixed(2), image:"https://images.unsplash.com/photo-1603808033192-082d6919d3e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80",description:"These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",porcent:prices[1],endprice:prices[2].toFixed(2),company:"SNEAKER COMPANY",
+    {id:0,name:"Fall Limited Edition Sneakers",price:prices[0].toFixed(2), image:"https://denis-munoz-baca.imgbb.com/",description:"These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",porcent:prices[1],endprice:prices[2].toFixed(2),company:"SNEAKER COMPANY",
     genre:genre[randomNumber(2)],
     }
 ]
